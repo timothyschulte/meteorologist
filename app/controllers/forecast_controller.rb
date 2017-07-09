7e762a1a77f2b7e4af38ba21a4b145f1854bdb7c
Tim Schulte <tschult0@chicagobooth.edu>
2017-07-09 00:34:35 +0000

    addressandweather1

--- app/controllers/forecast_controller.rb
+++ app/controllers/forecast_controller.rb
@@ -16,17 +16,30 @@ class ForecastController < ApplicationController
     # The longitude the user input is in the string @lng.
     # ==========================================================================
 
+url = "https://api.darksky.net/forecast/389c3b3d091ca54161e5e258e6d6eea1/37.8267,-122.4233"
+open(url).read
+
+parsed_data = JSON.parse(open(url).read)
+# latitude = parsed_data["results"][0]["geometry"]["location"]["lat"]
+# longitude = parsed_data["results"][0]["geometry"]["location"]["lng"]
+
+current_temperature = parsed_data["currently"]["temperature"]
+current_summary = parsed_data["currently"]["summary"]
+summary_of_next_sixty_minutes = parsed_data["minutely"]["summary"]
+summary_of_next_several_hours = parsed_data["hourly"]["summary"]
+summary_of_next_several_days = parsed_data["daily"]["summary"]
 
 
-    @current_temperature = "Replace this string with your answer."
 
-    @current_summary = "Replace this string with your answer."
+    @current_temperature = current_temperature
 
-    @summary_of_next_sixty_minutes = "Replace this string with your answer."
+    @current_summary = current_summary
 
-    @summary_of_next_several_hours = "Replace this string with your answer."
+    @summary_of_next_sixty_minutes = summary_of_next_sixty_minutes
 
-    @summary_of_next_several_days = "Replace this string with your answer."
+    @summary_of_next_several_hours = summary_of_next_several_hours
+
+    @summary_of_next_several_days = summary_of_next_several_days
 
     render("forecast/coords_to_weather.html.erb")
   end
